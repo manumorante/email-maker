69ee1972b86b4ace687f0ad22d5346fd8f670b47
Manu Morante <manuel.morante@razorfishhealthware.com>
2014-03-24 17:10:40 +0100

    test 1

---
+++ source/vars.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+module Vars
+
+	# Defined your online URL
+	APP_ONLINE = "ONLINE"
+
+	# Defined your local URL
+	APP_LOCAL = "LOCAL"
+
+	# Set current URL
+	APP_URL = APP_LOCAL
+
+	# DEFAULT FONT FAMLILY
+	DEFAULT_FONT_FAMILY = "Arial"
+
+end
